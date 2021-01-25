a85a49fa3785e566262091bd1b1efa5f154bccc5
Odhiambo Dormnic <ayimdomnic@outlook.com>
2021-01-25 21:26:51 +0300

    OOP class issues

--- OOP.js
+++ OOP.js
@@ -4,6 +4,31 @@ function Book(title, pages, isbn){
     this.isbn = isbn;
 }
 
+// Fun fact as of es6 javascript supports classes
+// so the above can be like so
+
+export class BookModel {
+
+    constructor(title, pages, isbn) {
+        this.title = title
+        this.pages = pages
+        this.isbn = isbn
+    }
+}
+
+// This defines values on the object that is required to
+// create the model
+// as per OOP, we can actually define static methods as well within the class whether public or private
+// I like this approach to JS cause it feels like you are writting server side code
+
+const values = {
+    title: "River and the source",
+    pages: "200",
+    isbn: "90893838UHX"
+}
+
+const newBook = new BookModel(...values)
+
 var book = new Book('title', 'pages', 'isbn');
 
 console.log(book.title);
