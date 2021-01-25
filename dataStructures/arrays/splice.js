eac35aadc910e28b0b09419eee615b9140064dbd
Odhiambo Dormnic <ayimdomnic@outlook.com>
2021-01-25 21:10:30 +0300

    refactor splice

--- dataStructures/arrays/splice.js
+++ dataStructures/arrays/splice.js
@@ -1,3 +1,6 @@
+// I like typescript because we can actually define the datatype here so
+// we waste no time dealing with type errors when we start the splice
+// e.g const splice = (array: Array) => { ...array}
 const spliceArray = (array) => {
-    array.splice(2, 0, 3,4,5)
+    array.splice(2, 0)
 }
